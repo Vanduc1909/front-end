ed6ea901cbf7254b6994d33f3a115a2da070998e
vanduc04 <buiduc1709@gmail.com>
2025-10-02 16:37:18 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,25 @@
-import { Button, Checkbox, Form, Input } from "antd";
-const onFinish = (values) => {
-  console.log("Success:", values);
-};
-const onFinishFailed = (errorInfo) => {
-  console.log("Failed:", errorInfo);
-};
 const Home = () => {
   return (
-    <div className="form-container">
-      <h1>Register</h1>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        style={{ maxWidth: 600 }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
+    <div>
+      <h1>Home</h1>
+      <button
+        className="btn btn-primary me-2"
+        onClick={() => (window.location.href = "/register")}
       >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item name="remember" valuePropName="checked" label={null}>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item label={null}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+        Register
+      </button>
+      <button
+        className="btn btn-success me-2"
+        onClick={() => (window.location.href = "/login")}
+      >
+        Login
+      </button>
+      <button
+        className="btn btn-info"
+        onClick={() => (window.location.href = "/dashboard")}
+      >
+        Dashboard
+      </button>
     </div>
   );
 };
