3d82cdce9633b4ce4d92c6a5bd4bb81522590ead
vanduc04 <buiduc1709@gmail.com>
2025-10-08 09:06:31 +0700

    abc

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    headers:{
-        "Content-Type":"application/json",
-    },
+  baseURL: "https://front-end-delta-ducduc.vercel.app/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 
 export default api;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   const onSubmit = async (data) => {
     const res = await api.post(`/login`, data);
     localStorage.setItem("user", JSON.stringify(res.data.user));
-    nav("/");
+    nav("/dashboard");
   };
 
   return (
