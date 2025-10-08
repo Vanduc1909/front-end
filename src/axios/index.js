8ff9678703b883b89ed581ec949b1d8143958d6e
vanduc04 <buiduc1709@gmail.com>
2025-10-08 09:21:30 +0700

    1

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://tts-t10.onrender.com",
+  baseURL: "https://tts-t10.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
