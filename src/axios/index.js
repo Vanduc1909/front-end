4a86d411d0f20d379fe3397fc2f9109890bcf9cd
vanduc04 <buiduc1709@gmail.com>
2025-10-08 09:17:32 +0700

    new

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://front-end-delta-ducduc.vercel.app/",
+  baseURL: "https://tts-t10.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
